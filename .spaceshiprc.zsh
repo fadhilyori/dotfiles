5b90bccc96574552ea347c4ffb86f3201ebc99a6
Fadhil Yori <me@fadhilyori.my.id>
2024-04-22 22:28:05 +0700

    Add .stow-local-ignore, .spaceshiprc.zsh, sysctl.d/99-custom.conf, and .zsh_aliases files

---
+++ .spaceshiprc.zsh
@@ -0,0 +1,27 @@
+# Display time
+SPACESHIP_TIME_SHOW=false
+
+# Display username always
+SPACESHIP_USER_SHOW=needed
+
+# Do not truncate path in repos
+SPACESHIP_DIR_TRUNC_REPO=false
+
+# Display when user have cached passwordless permission
+SPACESHIP_SUDO_SHOW=true
+
+SPACESHIP_PROMPT_ASYNC=false
+SPACESHIP_PROMPT_ADD_NEWLINE="true"
+SPACESHIP_CHAR_SYMBOL="=> "
+
+# Docker
+SPACESHIP_DOCKER_SHOW=true
+
+# Git
+SPACESHIP_GIT_SHOW=true
+SPACESHIP_GIT_BRANCH_SHOW=true
+SPACESHIP_GIT_STATUS_SHOW=true
+
+# Go
+SPACESHIP_GOLANG_SHOW=true
+
