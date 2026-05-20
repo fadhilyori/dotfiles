42d844df6ae037f110e67981a8b1036d6b5da645
Fadhil Yori <me@fadhilyori.my.id>
2026-05-20 19:36:25 +0700

    feat(zoxide): initialize zoxide for smarter directory navigation and override cd command

--- .config/fish/conf.d/zoxide.fish
+++ .config/fish/conf.d/zoxide.fish
@@ -1,4 +1,9 @@
 # Initialize zoxide for smarter directory navigation
 if command -v zoxide >/dev/null 2>&1
   zoxide init fish | source
+
+  # Override cd to use zoxide
+  function cd
+    command z $argv
+  end
 end
